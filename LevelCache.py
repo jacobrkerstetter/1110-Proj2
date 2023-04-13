1ec0981065ddb62aadb2fc7fd0425392362180ba
Jacob Kerstetter <83893427+jacobrkerstetter@users.noreply.github.com>
2023-04-13 12:22:40 -0400

    updating

--- LevelCache.py
+++ LevelCache.py
@@ -6,6 +6,8 @@
 # [ [valid (0/1), dirty (0/1), data] ], data, data, ...]
 # store newest data at the end, LRU is the front element and we will pop off
 
+from Block import block
+
 class LevelCache:
 
     def __init__(self, size, latency, blockSize, setAssociativity, writePolicy):
@@ -15,12 +17,19 @@ class LevelCache:
         self.setAssociativity = setAssociativity # set associativity in ways
         self.writePolicy = writePolicy # 0 = write-back, 1 = write-through
 
-        self.contents = [] # [ set 1 , set 2 , set 3 , set 4 ]
-                           # [ [ [block 1], [block 2], ... ] , [ [block 1], [block 2], ... ] ]
+        self.contents = [] # [ [ [block 1], [block 2], ... ] , [ [block 1], [block 2], ... ] ]
                            # block 1: [ valid , dirty , tag , data ]
 
+        self.fillEmptyContents()
+
     def fillEmptyContents(self):
-        return
+        layers = self.size // self.blockSize
+        for i in range(layers):
+            block = Block(0, 0, 0, 0)
+            for j in range(self.blockSize):
+                block.append([])
+
+            self.contents.append(block)
 
     def write(self, block):
         return
@@ -30,6 +39,12 @@ class LevelCache:
         blockOffset = address % self.blockSize
 
         # block address
-        setIndex = ( address // self.blockSize ) % self.setAssociativity
+        setIndex = ( address // self.blockSize ) % (self.size // self.blockSize)
        
-        print([blockOffset, setIndex])
+        if self.contents[setindex][blockOffset] == []:
+            
+
+cache = LevelCache(16, 1, 2, 1, 0)
+
+for i in range(16):
+    cache.read(i)
