560f61cb3a4ec5403147b06b1edb8bc29312f4f6
headhunter22 <jdh161@pitt.edu>
2023-04-13 12:44:48 -0400

    REad, write, constructor created in Cache

--- Cache.py
+++ Cache.py
@@ -19,35 +19,32 @@ class Cache:
         self.L1 = LevelCache(size, latency, blockSize, setAssociativity, writePolicy)
 
 # Create Cache #
-    def CreateLevelCache(self, level, size, latency, blockSize, setAssociativity, writePolicy):
-        self.level = level # level number of the cache
-        self.size = size # total capacity in bytes
-        self.latency = latency # access latency in cycles
-        self.blockSize = blockSize # block size in bytes
-        self.setAssociativity = setAssociativity # set associativity in ways
-        self.writePolicy = writePolicy # 0 = write-back, 1 = write-through
-
-        self.contents = [] # [ set 1 , set 2 , set 3 , set 4 ]
-                           # [ [ [block 1], [block 2], ... ] , [ [block 1], [block 2], ... ] ]
-                           # block 1: [ valid , dirty , tag , data ]
+    def CreateLevelCache(self, level):
+        level = LevelCache(self.size, self.latency, self.blockSize, self.setAssociativity, self.writePolicy)
 
 # function read (return hit/miss, latency, finish time)
     def read(self):
         if(self.L1.read(self, self.addr)):
             HITS = HITS + 1
-            Cache.printInfo(self, self.latency)
+            self.printInfo(self, self.latency)
+        else:
+            MISSES = MISSES + 1
 
 
 # function write
+    def write(self):
+       self.L1 = LevelCache.write()
+    
+
 # function print contents
-    def printInfo(self, latency):
-        #output all cache information
+    def printInfo(self):
+        # output all cache information
         print("Finish Time: ")
-        print("/nTotal Latency: " + latency)
+        print("/nTotal Latency: " + self.latency)
         print("/nCache Status (Only dirty bits): ")
         print("/nFinish Time: ")
-        print("/nHit rate: " + Cache.hitRate())
-        print("/nMiss rate: " + Cache.missRate())
+        print("/nHit rate: " + self.hitRate())
+        print("/nMiss rate: " + self.missRate())
 
 
 # function hit rate
