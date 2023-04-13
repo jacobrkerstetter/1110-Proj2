030b334ec6e016ffe151b5cb848883507cfc0290
headhunter22 <jdh161@pitt.edu>
2023-04-13 11:21:15 -0400

    Starting cache file

--- Cache.py
+++ Cache.py
@@ -2,8 +2,17 @@
 
 # all layers
 # static variable hits, misses, total accessses
+global HITS, MISSES, ACCESSES
+
+# Constructor #
+def __init__(self, op, address, ArrivingTime):
+    self.op = op
+    self.addr = address
+    self.ArrTime = ArrivingTime
 
 # function read (return hit/miss, latency, finish time)
+def read(self):
+    self.address
 # function write
 # function print contents
 # function hit rate
