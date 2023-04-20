cac68b86a20fd0587cb07c4348d11d304c7a0ed3
Jacob Kerstetter <83893427+jacobrkerstetter@users.noreply.github.com>
2023-04-20 12:48:26 -0400

    working on cache writes

--- Block.py
+++ Block.py
@@ -1,6 +1,7 @@
 class Block:
-    def __init__(self, valid, dirty, tag, data):
+    def __init__(self, valid, dirty, tag, data, blockOffset):
         self.valid = valid
         self.dirty = dirty
         self.tag = tag
         self.data = data
+        self.blockNum = blockOffset
--- Cache.py
+++ Cache.py
@@ -1,26 +1,34 @@
 from LevelCache import LevelCache
 # cache class
 
-# all layers
-# static variable hits, misses, total accessses
-global HITS, MISSES, ACCESSES
-
-# Constructor #
 class Cache:
+    # static variable hits, misses, total accessses
+    HITS, MISSES, ACCESSES = 0, 0, 0
+
+    # Constructor
     def __init__(self, layers, size, latency, blockSize, setAssociativity, writePolicy):
-        # self.size = size
+        self.layers = layers
+        L1 = LevelCache(size, latency, blockSize, setAssociativity, writePolicy)
+
+        self.cacheHierarchy = [L1]
+
         # self.latency = latency
         # self.blockSize = blockSize
         # self.setAssociativity = setAssociativity
         # self.writePolicy = writePolicy
-        self.layers = layers
-        self.L1 = LevelCache(size, latency, blockSize, setAssociativity, writePolicy)
 
-# Create Cache
-    def CreateLevelCache(self, level):
-        level = LevelCache(self.size, self.latency, self.blockSize, self.setAssociativity, self.writePolicy)
+    # Create New Level
+    def CreateLevelCache(self, size, latency, blockSize, setAssociativity, writePolicy):
+        # create new level in the cache
+        level = LevelCache(size, latency, blockSize, setAssociativity, writePolicy)
 
-# function read (return hit/miss, latency, finish time)
+        # set the last level flag to false in the previously LLC
+        self.cacheHierarchy[len(self.cacheHierarchy) - 1] = False
+
+        # append new cache to the hierarchy
+        self.cacheHierarchy.append(level)
+
+    # Function read (return hit/miss, latency, finish time)
     def read(self):
         if(self.L1.read(self, self.addr)):
             HITS = HITS + 1
@@ -28,28 +36,61 @@ class Cache:
         else:
             MISSES = MISSES + 1
 
-# function write
-    def write(self):
-       self.L1 = LevelCache.write()
-    
+    # Function write
+    def write(self, address, arrivingTime):
+        # consider write-thru and write-back for each level
 
-# function print contents
+        for cache in self.cacheHierarchy:
+            # if this cache is write-back
+            if cache.writePolicy == 0:
+                # if level cache hits on write
+                if cache.read(address) == 1:
+                    # write data into cache and mark as dirty
+                    cache.write(address, 1)
+                    # if there are lower level caches, continue loop and write there as well
+
+                # if level cache misses on write
+                else:
+                    # allocate space for new line (may require eviction)
+                    cache.write(address, 0)
+
+            # if this cache is write-thru
+            else:
+                # if cache hits, write to next level as well
+                if cache.read(address) == 1:
+                    cache.write(address, 0)
+
+                # if cache misses
+        
+
+
+    # Function print contents
     def printInfo(self):
         # output all cache information
         print("Finish Time: ")
-        print("/nTotal Latency: " + self.latency)
-        print("/nCache Status (Only dirty bits): ")
-        print("/nFinish Time: ")
-        print("/nHit rate: " + self.hitRate())
-        print("/nMiss rate: " + self.missRate())
+        print("Cache Status (Only dirty bits): ")
+        print("Finish Time: ")
+        print("Hit rate: " + str(self.hitRate()))
+        print("Miss rate: " + str(self.missRate()))
+
+        for cache in self.cacheHierarchy:
+            cache.printContents()
 
 
-# function hit rate
-    def hitRate():
-        hitRate = HITS / (HITS + MISSES)
+    # Function hit rate
+    def hitRate(self):
+        try:
+            hitRate = Cache.HITS / (Cache.HITS + Cache.MISSES)
+        except:
+            hitRate = 0
+
         print(hitRate)
 
-# function miss rate
-    def missRate():
-        missRate = MISSES / (HITS + MISSES)
+    # Function miss rate
+    def missRate(self):
+        try:
+            missRate = Cache.MISSES / (Cache.HITS + Cache.MISSES)
+        except:
+            missRate = 0
+
         print(missRate)
